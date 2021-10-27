bb0e3e99ad4bb1a78783df4b6970b94170a07f9c
tcho <teresacho135@gmail.com>
2021-10-27 11:38:45 -0700

    installs/setup concurrently + nodemon

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const products = require('./data/products');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('API IS RUNNING... ')
+    res.send('API IS RUNNING..... ')
 })
 
 app.get('/api/products', (req, res) => {
